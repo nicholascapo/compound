c5d5f84a945285cd7bf4cfe1869f79a90b33ecce
Nicholas Capo <nicholas.capo@gmail.com>
2019-07-29 15:47:50 -0500

    Change title in each site

--- copy_site.sh
+++ copy_site.sh
@@ -5,4 +5,5 @@ rm -f site/package-lock.json site/yarn.lock
 for dir in lerna_npm lerna_yarn lerna_yarn_workspaces yarn_workspaces; do
   rm -rf ${dir}/site
   cp -R ./site ./${dir}/
+  sed -i "s/COMPOUND/${dir^^}/" ${dir}/site/src/components/layout.js
 done
