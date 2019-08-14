f686cd62b6884e788ca5508515da53c61c15c39e
Nicholas Capo <nicholas.capo@gmail.com>
2019-08-14 13:18:50 -0500

    add npm site

---
+++ npm/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { Glasses } from "../components/image"
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <Glasses />
+  </Layout>
+)
+
+export default IndexPage
